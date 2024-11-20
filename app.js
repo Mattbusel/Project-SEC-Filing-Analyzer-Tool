2d60f9959b3e04b316dd55dc2c26adeaf53d54e9
Matthew Busel <mattbusel@gmail.com>
2024-11-19 19:14:27 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AnalysisDashboard from './components/MarketAnalysis/AnalysisDashboard';  // Make sure this path matches exactly
+
+function App() {
+  return (
+    <div className="App">
+      <AnalysisDashboard />
+    </div>
+  );
+}
+
+export default App;
